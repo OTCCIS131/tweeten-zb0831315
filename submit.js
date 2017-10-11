2f08218fa3f1570f7499afd0dfd82a75f0b46461
zb0831315 <zb0831315@otc.edu>
2017-10-11 13:20:31 -0500

    Added second tweet area

---
+++ submit.js
@@ -0,0 +1,20 @@
+// $(function) (){
+//     $('#idOrClassHere').submit (e) {
+//         e.preventDefault()
+           // 
+
+//         let newA = $("[name=title]", $(this)).val()
+//         console.log("Text here", newA)
+           // Create a new element to insert into the page
+           // let newElement = document.createElement("li")
+           // let newElement $("<li>")
+           // Modify said element so it looks the same as the others
+           // newElement.addClass('list-group-item')
+           // newElement.text(newMovie)
+           // Insert into the page
+
+           // $("#movieList").append(newElement)
+
+
+//     })
+// })
